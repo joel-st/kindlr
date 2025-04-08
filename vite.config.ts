a1a5a036f0879246b146ded8b5e008c97b8e4c14
Lone <17547231+joel-st@users.noreply.github.com>
2025-04-08 18:17:06 +0100

    fix paths, use bun in deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -27,17 +27,17 @@ export default defineConfig({
         name: "Kindlr",
         theme_color: "#8200db",
         icons: [
-          { src: "/favicon.ico", type: "image/x-icon", sizes: "16x16 32x32" },
-          { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
-          { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
+          { src: "favicon.ico", type: "image/x-icon", sizes: "16x16 32x32" },
+          { src: "icon-192.png", type: "image/png", sizes: "192x192" },
+          { src: "icon-512.png", type: "image/png", sizes: "512x512" },
           {
-            src: "/icon-192-maskable.png",
+            src: "icon-192-maskable.png",
             type: "image/png",
             sizes: "192x192",
             purpose: "maskable",
           },
           {
-            src: "/icon-512-maskable.png",
+            src: "icon-512-maskable.png",
             type: "image/png",
             sizes: "512x512",
             purpose: "maskable",
